0acb05aa8ed40e08f7ec1fad0fb8bfcbdf5e20d8
rishi921 <shrishidurga21@gmail.com>
2023-11-28 11:31:09 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+    </div>
+  );
+};
+
+export default Footer;
